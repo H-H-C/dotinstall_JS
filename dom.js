595f6bb70772e538d3481eb631ca56aa61fe6663
honghong <90856538+H-H-C@users.noreply.github.com>
2021-10-27 16:30:05 +0900

    dotinstall_DOM

---
+++ dom.js
@@ -0,0 +1,11 @@
+
+'use script';
+
+{
+  document.querySelector('button').addEventListener('click', () => 
+  {
+    const item1 = document.querySelectorAll('li')[1];
+    item1.remove();
+
+  });
+}
